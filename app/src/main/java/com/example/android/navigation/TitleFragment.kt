6cc0fdf883fb4fe5896c685c3e3dbfc60fa25793
yosrri <yosrri@github.com>
2023-07-06 13:49:01 +0300

    Getting the beacon app up and running

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -29,6 +30,9 @@ class TitleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener{
+                view: View -> Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 
